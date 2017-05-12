6550c904c16d1c804e145294ad28315c1199d4cf
davidhaughton14 <2080734h@student.gla.ac.uk>
2017-05-12 09:35:59 +0100

    completed 1-4

--- ap-revision/Chapter1/temp.c
+++ ap-revision/Chapter1/temp.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
-/* print Fahrenheit-Celsius table for fahr = 0, 20, ..., 300; floating-point version */
 int main()
 {
   float fahr, celsius;
   float lower, upper, step;
-  lower = 0; /* lower limit of temperatuire scale */
-  upper = 300; /* upper limit */
+  lower = 0.0; /* lower limit of temperatuire scale */
+  upper = 300.0; /* upper limit */
   step = 20; /* step size */
-  fahr = lower;
-  printf("%s\t%s","Fahr", "Cels\n");
-  while (fahr <= upper) {
-    celsius = (5.0/9.0) * (fahr-32.0);
-    printf("%3.0f\t%6.1f\n", fahr, celsius);
-    fahr = fahr + step;
+  celsius = lower;
+  printf("%s\t%s","Cels", "Fahr\n");
+  while (celsius <= upper) {
+    fahr = ((celsius*9)/5) + 32;
+    printf("%3.0f\t%6.1f\n", celsius, fahr);
+    celsius = celsius + step;
   }
 }
